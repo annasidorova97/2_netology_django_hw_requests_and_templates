1bb0a3d574b195e25851de6e445a46d04aba7581
annasidorova97 <sidorova_aa@icloud.com>
2023-09-07 23:06:29 +0300

    Finish landing

--- landing/app/views.py
+++ landing/app/views.py
@@ -2,31 +2,33 @@ from collections import Counter
 
 from django.shortcuts import render
 
-# Для отладки механизма ab-тестирования используйте эти счетчики
-# в качестве хранилища количества показов и количества переходов.
-# но помните, что в реальных проектах так не стоит делать
-# так как при перезапуске приложения они обнулятся
 counter_show = Counter()
 counter_click = Counter()
 
 
 def index(request):
-    # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
-    return render(request, 'index.html')
+    if request.GET.get('from-landing') == 'original':
+        counter_click['original_click'] += 1
+        return render(request, 'index.html')
+    elif request.GET.get('from-landing') == 'test':
+        counter_click['test_click'] += 1
+        return render(request, 'index.html')
+    else:
+        return render(request, 'index.html')
+
 
 
 def landing(request):
-    # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
-    # в зависимости от GET параметра ab-test-arg
-    # который может принимать значения original и test
-    # Так же реализуйте логику подсчета количества показов
-    return render(request, 'landing.html')
+    if request.GET.get('ab-test-arg') == 'original' or request.GET.get('ab-test-arg') == None:
+        counter_show['original_show'] += 1
+        return render(request, 'landing.html')
+    else:
+        counter_show['test_show'] += 1
+        return render(request, 'landing_alternate.html')
 
 
 def stats(request):
-    # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
-    # Для вывода результат передайте в следующем формате:
     return render(request, 'stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': counter_click['test_click'] / counter_show['test_show'],
+        'original_conversion': counter_click['original_click'] / counter_show['original_show'],
     })
