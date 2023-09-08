0f1f49c29aaaff0fb972a0bd6f12e47d23c27ffb
annasidorova97 <sidorova_aa@icloud.com>
2023-09-09 01:13:38 +0300

    Add abs_path_to_file function. Fixed bus_stations view

--- pagination/app/views.py
+++ pagination/app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+import os
 
-from pagination.functions import get_list_of_dicts
+from pagination.functions import get_list_of_dicts, abs_path_to_file
 
 
 def index(request):
@@ -11,12 +12,9 @@ def index(request):
 def bus_stations(request):
     current_page = 1
     next_page_url = 'write your url'
-    bus_stations_list = get_list_of_dicts(r'D:\projects\django_netology\2_netology_django_hw_requests_and_templates\pagination\data-398-2018-08-30.csv', list_of_dicts=None)
+    bus_stations_list = get_list_of_dicts(abs_path_to_file(r'pagination\data-398-2018-08-30.csv'), list_of_dicts=None)
     return render(request, 'index.html', context={
         'bus_stations': bus_stations_list,
-
-        # 'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'},
-        #                  {'Name': 'другое название', 'Street': 'другая улица', 'District': 'другой район'}],
         'current_page': current_page,
         'prev_page_url': None,
         'next_page_url': next_page_url,
--- pagination/functions.py
+++ pagination/functions.py
@@ -1,3 +1,4 @@
+import os
 from csv import DictReader
 
 
@@ -7,5 +8,11 @@ def get_list_of_dicts(csv_file, list_of_dicts=None):
     with open(csv_file, newline='') as csvfile:
         reader = DictReader(csvfile)
         for row in reader:
-            list_of_dicts.append({row['Name'], row['Street'], row['District']})
+            list_of_dicts.append({'Name': row['Name'], 'Street': row['Street'], 'District': row['District']})
     return list_of_dicts
+
+
+def abs_path_to_file(path: str):
+    current_path = os.getcwd()
+    full_path = '\\'.join([current_path, path])
+    return full_path
